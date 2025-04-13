8666b7cbebb1f2a36c975d65d0ac114a9bb24f35
dsad233 <ksl1421@naver.com>
2025-04-13 20:34:11 +0900

    fix: 미들웨어 구동 오류 수정

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { limiter } from './middlewares/express-limited';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-
   app.use(limiter);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/middlewares/express-limited.ts
+++ src/middlewares/express-limited.ts
@@ -5,4 +5,10 @@ export const limiter = rateLimit({
 	limit: 5, // 100분에 대한 요청 할당량 추적
 	standardHeaders: 'draft-8', //  CORS draft-8 규약에 따라 응답 헤더를 생성
 	legacyHeaders: true, // 오래된 응답 헤더 방식을 수용하지 않음.
+	message: "요청이 너무 많습니다. 잠시 후 다시시도 해주세요.",
+	handler: (req, res, next, options) =>
+		res.status(options.statusCode).json({
+			success: false,
+      		message: options.message,
+		}),
 })
